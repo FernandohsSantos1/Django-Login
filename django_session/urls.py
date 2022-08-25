52fac9dc5ad900952b7d3b8fd04e32d34706dba2
Fernando <hssfernando@hotmail.com>
2022-08-25 10:13:52 -0300

    Pasta principal

---
+++ django_session/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('usuarios.urls')),
+    path('', lambda request: redirect('auth/login/')),
+    path('plataforma/', include('plataforma.urls'))
+]
